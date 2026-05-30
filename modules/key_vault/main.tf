baa4107dbed3c020583e87404a6fef2da2a6542e
97ajaykumaryadav <97ajaykumaryadav@gmail.com>
2026-05-30 18:01:46 +0530

    Switch Key Vault to RBAC authorization model to support IAM roles

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -14,38 +14,8 @@ resource "azurerm_key_vault" "kv" {
   soft_delete_retention_days  = each.value.soft_delete_retention_days
   purge_protection_enabled     = each.value.purge_protection_enabled
   public_network_access_enabled = each.value.public_network_access_enabled
+  enable_rbac_authorization   = true # Enable modern RBAC model
   tags                        = each.value.tags
 }
 
-locals {
-  # Combine deployer ID and admin IDs, then remove duplicates
-  all_admin_ids = distinct(concat([data.azurerm_client_config.current.object_id], var.admin_object_ids))
-
-  all_policies = flatten([
-    for kv_key, kv in azurerm_key_vault.kv : [
-      for obj_id in local.all_admin_ids : {
-        kv_key = kv_key
-        kv_id  = kv.id
-        obj_id = obj_id
-      }
-    ]
-  ])
-}
-
-resource "azurerm_key_vault_access_policy" "admin_access" {
-  for_each = { for ap in local.all_policies : "${ap.kv_key}.${ap.obj_id}" => ap }
-
-  key_vault_id = each.value.kv_id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = each.value.obj_id
-
-  secret_permissions = [
-    "Get",
-    "List",
-    "Set",
-    "Delete",
-    "Purge",
-    "Recover",
-    "Restore"
-  ]
-}
+# No longer need azurerm_key_vault_access_policy when using enable_rbac_authorization = true
